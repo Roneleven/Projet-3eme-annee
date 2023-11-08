8b2e4244eff916c43e8f8da1136ed5e85773050b
Wood <wassim.boulatika@gmail.com>
2023-11-08 18:55:40 +0100

    added zone behaviors, and bumpers

--- Assets/Script/Mod_Zone.cs
+++ Assets/Script/Mod_Zone.cs
@@ -4,29 +4,13 @@ using UnityEngine;
 
 public class Mod_Zone : MonoBehaviour
 {
-    //this script will be on the object that represents the zone, and when the player enters the zone, it will send a debug
+    public GameObject player;
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject == player)
         {
-            Debug.Log("Player has entered the zone");
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            Debug.Log("Player has left the zone");
-        }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            Debug.Log("Player is in the zone");
+            // Modifie les propriétés du joueur
         }
     }
 
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,29 +7,82 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed;
-    public float gravity = -9.81f; // Gravité de base
+    public float gravity = -9.81f; // Base gravity
     public float jumpHeight;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+    public LayerMask zone1Mask;
+    public LayerMask zone2Mask;
+    public float gravityZone1;
+    public float speedZone1;
     private float movementX;
     private float movementY;
-    public int speedIncrement = 0; // 0, 1, 2, 3
-    public int gravityIncrement = 0; // 0, 1, 2, 3
-    public int incrementalValue = 1; // Valeur d'incrémentation
+    public float bumperJumpHeight;
+
 
     Vector3 velocity;
     bool isGrounded;
 
-    // Tableaux de valeurs à modifier
-    public float[] speedValues = { 10f, 15f, 20f, 25f, 30f };
-    public float[] gravityValues = { -9.81f, -9.5f, -9.0f, -8.5f, -8.0f };
+    // Zone variables
+    public bool inZone1 = false;
+    public bool inZone2 = false;
+
+    private float defaultSpeed;
+    private float defaultGravity;
+
+    void Start()
+    {
+        defaultSpeed = speed;
+        defaultGravity = gravity;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        // Check if player is in zone 1
+        if (Physics.CheckSphere(groundCheck.position, groundDistance, zone1Mask))
+        {
+            inZone1 = true;
+        }
+        else
+        {
+            inZone1 = false;
+        }
+
+        // Check if player is in zone 2
+        if (Physics.CheckSphere(groundCheck.position, groundDistance, zone2Mask))
+        {
+            inZone2 = true;
+        }
+        else
+        {
+            inZone2 = false;
+        }
+
+        // Apply zone effects
+        if (inZone1)
+        {
+            gravity = gravityZone1; 
+            speed = speedZone1; 
+        }
+        else if (inZone2)
+        {
+            jumpHeight = 0f;
+            gravity = defaultGravity; 
+            speed = defaultSpeed; 
+        }
+        else
+        {
+            gravity = defaultGravity; 
+            speed = defaultSpeed; 
+            jumpHeight = 2f; 
+        }
+
+        // Check if grounded
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        // Apply gravity and movement
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
@@ -37,15 +90,13 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * movementX + transform.forward * movementY;
 
-        float modifiedSpeed = speedValues[speedIncrement];
-        float modifiedGravity = gravityValues[gravityIncrement];
+        controller.Move(move.normalized * speed * Time.deltaTime);
 
-        controller.Move(move.normalized * modifiedSpeed * Time.deltaTime);
-
-        velocity.y += modifiedGravity * Time.deltaTime;
+        velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
 
+    // Input handling methods
     private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
@@ -55,61 +106,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnJump(InputValue jumpValue)
     {
-        if (isGrounded)
+        if (isGrounded && !inZone2)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
     }
 
-    public void IncrementSpeed()
+    void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (speedIncrement < 4)
+        if (hit.collider.CompareTag("Bumper"))
         {
-            speedIncrement++;
+            float originalJumpHeight = jumpHeight;
+            jumpHeight = bumperJumpHeight;
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            jumpHeight = originalJumpHeight;
         }
     }
 
-    public void DecrementSpeed()
-    {
-        if (speedIncrement > 0) 
-        {
-            speedIncrement--;
-        }
-    }
-
-    public void IncrementGravity()
-    {
-        if (gravityIncrement < 4)
-        {
-            gravityIncrement++;
-        }
-    }
-
-    public void DecrementGravity()
-    {
-        if (gravityIncrement > 0)
-        {
-            gravityIncrement--;
-        }
-    }
-
-    private void OnSpeedIncrement()
-    {
-        IncrementSpeed();
-    }
-
-    private void OnSpeedDecrement()
-    {
-        DecrementSpeed();
-    }
-
-    private void OnGravityIncrement()
-    {
-        IncrementGravity();
-    }
-
-    private void OnGravityDecrement()
-    {
-        DecrementGravity();
-    }
 }

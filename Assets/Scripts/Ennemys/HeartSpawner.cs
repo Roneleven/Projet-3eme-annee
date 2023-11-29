c675943ec98a0b147e8488e10ae2960e3b185804
Burizof <maloneetenorba@outlook.com>
2023-11-29 19:20:16 +0100

    FixCage

--- Assets/Scripts/Ennemys/HeartSpawner.cs
+++ Assets/Scripts/Ennemys/HeartSpawner.cs
@@ -81,8 +81,8 @@ else
 
 private IEnumerator SpawnTransparentAndRealCube(Vector3 spawnPosition) // Nouvelle coroutine
 {
-    GameObject transparentCube = Instantiate(transparentCubePrefab, spawnPosition, Quaternion.identity, spawnContainer.transform); // Instanciez le cube semi-transparent
-        yield return new WaitForSeconds(1); // Attendez une seconde
+    GameObject transparentCube = Instantiate(transparentCubePrefab, spawnPosition, Quaternion.identity); // Instanciez le cube semi-transparent
+    yield return new WaitForSeconds(1); // Attendez une seconde
 
     Collider[] colliders = Physics.OverlapSphere(spawnPosition, gridSize / 2);
     bool playerInPosition = false;
@@ -99,8 +99,8 @@ private IEnumerator SpawnTransparentAndRealCube(Vector3 spawnPosition) // Nouvel
 
     if (!playerInPosition)
     {
-            Instantiate(cubePrefab, spawnPosition, Quaternion.identity, spawnContainer.transform); // Instanciez le cube réel
-        }
+        Instantiate(cubePrefab, spawnPosition, Quaternion.identity); // Instanciez le cube réel
+    }
     else
     {
         Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
@@ -117,8 +117,8 @@ private IEnumerator SpawnTransparentAndRealCube(Vector3 spawnPosition) // Nouvel
                     Vector3 cubePosition = new Vector3(x, y, z);
                     if (Mathf.Abs(x - playerPosition.x) >= 3 || Mathf.Abs(y - playerPosition.y) >= 3 || Mathf.Abs(z - playerPosition.z) >= 3)
                     {
-                            Instantiate(cubePrefab, spawnPosition, Quaternion.identity, spawnContainer.transform); // Instanciez le cube réel
-                        }
+                        Instantiate(cubePrefab, cubePosition, Quaternion.identity); // Instanciez le cube réel
+                    }
                 }
             }
         }

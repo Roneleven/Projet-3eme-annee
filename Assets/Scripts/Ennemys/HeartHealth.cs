4af0b4771a33f65b4b9ad3e93ff2c9bbcd1f897d
Wood <wassim.boulatika@gmail.com>
2023-11-30 16:20:10 +0100

    added palier feature for heart

--- Assets/Scripts/Ennemys/HeartHealth.cs
+++ Assets/Scripts/Ennemys/HeartHealth.cs
@@ -4,12 +4,14 @@ public class HeartHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     public int health = 100;
+    public int currentPalier = 1;
     public Transform[] teleportPositions; // Tableau des positions de téléportation
-    private int lastTeleportIndex = -1; 
+    private int lastTeleportIndex = -1;
+    private HeartSpawner heartSpawner;
 
     private void Start()
     {
-        
+        heartSpawner = FindObjectOfType<HeartSpawner>();
     }
 
     public void TakeDamage(int damage)
@@ -36,6 +38,12 @@ public class HeartHealth : MonoBehaviour
             transform.position = nextTeleportPosition.position;
 
             health = maxHealth;
+
+            // Ajouter cette ligne pour déclencher le changement de palier
+            if (heartSpawner != null)
+            {
+                heartSpawner.ChangePalierOnTeleport();
+            }
         }
     }
 }
--- Assets/Scripts/Ennemys/HeartSpawner.cs
+++ Assets/Scripts/Ennemys/HeartSpawner.cs
@@ -6,150 +6,189 @@ public class HeartSpawner : MonoBehaviour
 {
     public GameObject cubePrefab;
     public float spawnInterval = 1f;
-    public float spawnRadius = 4f;
+    public float spawnRadius;
     public GameObject spawnContainer;
     public float gridSize = 1f;
     public float exclusionRadius = 2f;
-    public float spawnCount = 1; // Nombre de cubes à faire apparaître à chaque intervalle
+    public float spawnCount;
     public GameObject transparentCubePrefab;
     public HeartHealth heartHealth;
+    public int currentPalier = 1;
+    public float temporarySpawnCount;
+    public float timeTemporaryPalier;
+
+    private bool isCooldownActive = false;
 
     private void Start()
     {
         StartCoroutine(SpawnCube());
-        StartCoroutine(ChangeSpawnRadiusAndCount());
     }
 
-private IEnumerator SpawnCube()
-{
-    while (true)
+    private IEnumerator SpawnCube()
     {
-        for (int i = 0; i < spawnCount; i++)
+        while (true)
         {
-            Vector3 spawnPosition;
-            do
+            for (int i = 0; i < spawnCount; i++)
             {
-                spawnPosition = Random.insideUnitSphere * spawnRadius;
-            }
-            while (spawnPosition.magnitude < exclusionRadius);
-
-            spawnPosition /= gridSize;
-            spawnPosition = new Vector3(Mathf.Round(spawnPosition.x), Mathf.Round(spawnPosition.y), Mathf.Round(spawnPosition.z));
-            spawnPosition *= gridSize;
-
-            spawnPosition += transform.position;
-
-            Collider[] colliders = Physics.OverlapSphere(spawnPosition, gridSize / 2);
-            if (colliders.Length > 0)
-            {
-                bool playerInPosition = false;
-                foreach (Collider collider in colliders)
+                Vector3 spawnPosition;
+                do
                 {
-                    if (collider.gameObject.tag == "Player")
+                    spawnPosition = Random.insideUnitSphere * spawnRadius;
+                } while (spawnPosition.magnitude < exclusionRadius);
+
+                spawnPosition /= gridSize;
+                spawnPosition = new Vector3(Mathf.Round(spawnPosition.x), Mathf.Round(spawnPosition.y), Mathf.Round(spawnPosition.z));
+                spawnPosition *= gridSize;
+
+                spawnPosition += transform.position;
+
+                Collider[] colliders = Physics.OverlapSphere(spawnPosition, gridSize / 2);
+                if (colliders.Length > 0)
+                {
+                    bool playerInPosition = false;
+                    foreach (Collider collider in colliders)
                     {
-                        playerInPosition = true;
-                        break;
-                    }
-                    CubeHealth cubeHealth = collider.gameObject.GetComponent<CubeHealth>();
-                    if (cubeHealth != null)
-                    {
-                        if (cubeHealth.health < 26)
+                        if (collider.gameObject.tag == "Player")
                         {
-                            cubeHealth.health += 5;
+                            playerInPosition = true;
                             break;
                         }
-                        else
+                        CubeHealth cubeHealth = collider.gameObject.GetComponent<CubeHealth>();
+                        if (cubeHealth != null)
                         {
-                            continue;
+                            if (cubeHealth.health < 26)
+                            {
+                                cubeHealth.health += 5;
+                                break;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                    }
+                    if (playerInPosition)
+                    {
+                        StartCoroutine(SpawnTransparentAndRealCube(spawnPosition));
+                    }
+                }
+                else
+                {
+                    StartCoroutine(SpawnTransparentAndRealCube(spawnPosition));
+                }
+            }
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    private IEnumerator SpawnTransparentAndRealCube(Vector3 spawnPosition)
+    {
+        GameObject transparentCube = Instantiate(transparentCubePrefab, spawnPosition, Quaternion.identity);
+        yield return new WaitForSeconds(1);
+
+        Collider[] colliders = Physics.OverlapSphere(spawnPosition, gridSize / 2);
+        bool playerInPosition = false;
+        foreach (Collider collider in colliders)
+        {
+            if (collider.gameObject.tag == "Player")
+            {
+                playerInPosition = true;
+                break;
+            }
+        }
+
+        Destroy(transparentCube);
+
+        if (!playerInPosition)
+        {
+            Instantiate(cubePrefab, spawnPosition, Quaternion.identity, spawnContainer.transform);
+        }
+        else
+        {
+            Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+            playerPosition /= gridSize;
+            playerPosition = new Vector3(Mathf.Round(playerPosition.x), Mathf.Round(playerPosition.y), Mathf.Round(playerPosition.z));
+            playerPosition *= gridSize;
+
+            for (float x = playerPosition.x - 3; x <= playerPosition.x + 3; x += gridSize)
+            {
+                for (float y = playerPosition.y - 3; y <= playerPosition.y + 3; y += gridSize)
+                {
+                    for (float z = playerPosition.z - 3; z <= playerPosition.z + 3; z += gridSize)
+                    {
+                        Vector3 cubePosition = new Vector3(x, y, z);
+                        if (Mathf.Abs(x - playerPosition.x) >= 3 || Mathf.Abs(y - playerPosition.y) >= 3 || Mathf.Abs(z - playerPosition.z) >= 3)
+                        {
+                            Instantiate(cubePrefab, cubePosition, Quaternion.identity, spawnContainer.transform);
                         }
                     }
                 }
-                if (playerInPosition)
-                {
-                    StartCoroutine(SpawnTransparentAndRealCube(spawnPosition)); // Appeler la nouvelle coroutine
-                }
-            }
-else
-{
-    StartCoroutine(SpawnTransparentAndRealCube(spawnPosition)); // Appeler la nouvelle coroutine
-}
-        }
-
-        yield return new WaitForSeconds(spawnInterval);
-    }
-}
-
-private IEnumerator SpawnTransparentAndRealCube(Vector3 spawnPosition) // Nouvelle coroutine
-{
-    GameObject transparentCube = Instantiate(transparentCubePrefab, spawnPosition, Quaternion.identity); // Instanciez le cube semi-transparent
-    yield return new WaitForSeconds(1); // Attendez une seconde
-
-    Collider[] colliders = Physics.OverlapSphere(spawnPosition, gridSize / 2);
-    bool playerInPosition = false;
-    foreach (Collider collider in colliders)
-    {
-        if (collider.gameObject.tag == "Player")
-        {
-            playerInPosition = true;
-            break;
-        }
-    }
-
-    Destroy(transparentCube); // Détruisez le cube semi-transparent
-
-    if (!playerInPosition)
-    {
-        Instantiate(cubePrefab, spawnPosition, Quaternion.identity, spawnContainer.transform); // Instanciez le cube réel
-    }
-    else
-    {
-        Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
-        playerPosition /= gridSize;
-        playerPosition = new Vector3(Mathf.Round(playerPosition.x), Mathf.Round(playerPosition.y), Mathf.Round(playerPosition.z));
-        playerPosition *= gridSize;
-
-        for (float x = playerPosition.x - 3; x <= playerPosition.x + 3; x += gridSize)
-        {
-            for (float y = playerPosition.y - 3; y <= playerPosition.y + 3; y += gridSize)
-            {
-                for (float z = playerPosition.z - 3; z <= playerPosition.z + 3; z += gridSize)
-                {
-                    Vector3 cubePosition = new Vector3(x, y, z);
-                    if (Mathf.Abs(x - playerPosition.x) >= 3 || Mathf.Abs(y - playerPosition.y) >= 3 || Mathf.Abs(z - playerPosition.z) >= 3)
-                    {
-                        Instantiate(cubePrefab, cubePosition, Quaternion.identity, spawnContainer.transform); // Instanciez le cube réel
-                    }
-                }
             }
         }
     }
-}
 
-    private IEnumerator ChangeSpawnRadiusAndCount()
+    public void ChangePalierOnTeleport()
     {
-        yield return new WaitForSeconds(30);
-        spawnRadius = 8;
-        spawnCount = 4; // Faire apparaître 2 cubes à chaque intervalle après 30 secondes
+        if (isCooldownActive) return;
 
-        yield return new WaitForSeconds(60);
-        spawnRadius = 12;
-        spawnCount = 8;
-
-        yield return new WaitForSeconds(90);
-        spawnRadius = 16;
-        spawnCount = 16;
-
-        yield return new WaitForSeconds(120);
-        spawnRadius = 20;
-        spawnCount = 32;
-
-        yield return new WaitForSeconds(150);
-        spawnRadius = 24;
-        spawnCount = 48;
-
-        yield return new WaitForSeconds(180);
-        spawnRadius = 28;
-        spawnCount = 64;
+        if (heartHealth != null)
+        {
+            StartCoroutine(ResetPalier());
+        }
     }
 
+
+    private IEnumerator ResetPalier()
+    {
+        isCooldownActive = true;
+
+        float originalSpawnRadius = spawnRadius;
+        float originalSpawnCount = spawnCount;
+
+        spawnCount = temporarySpawnCount;
+
+        for (int palier = 1; palier <= currentPalier; palier++)
+        {
+            float temporarySpawnCountBeforeAdjust = spawnCount;
+            AdjustPalierValues(palier);
+            spawnCount = temporarySpawnCountBeforeAdjust;
+            yield return new WaitForSeconds(timeTemporaryPalier);
+        }
+
+        // Restaurer les valeurs originales
+        spawnRadius = originalSpawnRadius;
+        spawnCount = originalSpawnCount;
+
+        // Passer au palier suivant
+        currentPalier++;
+        AdjustPalierValues(currentPalier);
+
+        isCooldownActive = false;
+    }
+
+
+    private void AdjustPalierValues(int palier)
+    {
+        if (palier == 1)
+        {
+            spawnRadius = 4; // Changer en fonction du palier 1
+            spawnCount = 6; // Changer en fonction du palier 1
+        }
+        else if (palier == 2)
+        {
+            spawnRadius = 8; // Changer en fonction du palier 2
+            spawnCount = 12; // Changer en fonction du palier 2
+        }
+        else if (palier == 3)
+        {
+            spawnRadius = 12; // Changer en fonction du palier 3
+            spawnCount = 25; // Changer en fonction du palier 3
+        }
+        else if (palier == 4)
+        {
+            spawnRadius = 16; // Changer en fonction du palier 3
+            spawnCount = 35; // Changer en fonction du palier 3
+        }
+    }
 }

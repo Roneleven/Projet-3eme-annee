dadb383aa545ec24efdf55d2c655de7e0449cf43
Wood <wassim.boulatika@gmail.com>
2023-12-17 17:02:43 +0100

    finished jetpack behaviors

--- Assets/Scripts/Player/PlayerMovementsRB.cs
+++ Assets/Scripts/Player/PlayerMovementsRB.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,15 +13,22 @@ public class PlayerMovementsRB : MonoBehaviour
     public float bumperJumpHeight;
     public Transform respawnPoint;
 
+    public float jetpackForce = 10f;
+    public float jetpackChargeRate = 1f;
+    public float maxJetpackCharge = 100f;
+
     private Rigidbody rb;
     private float movementX;
     private float movementY;
     private bool isGrounded;
+    public float jetpackCharge;
+    public InputActionReference jetpack;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        jetpackCharge = maxJetpackCharge;
     }
 
     private void Update()
@@ -32,18 +38,32 @@ public class PlayerMovementsRB : MonoBehaviour
         // Check if grounded
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        // Recharge du jetpack au sol
+        if (isGrounded)
+        {
+            jetpackCharge = Mathf.Min(jetpackCharge + jetpackChargeRate * Time.deltaTime, maxJetpackCharge);
+        }
+
         Vector3 move = transform.right * movementX + transform.forward * movementY;
+        rb.MovePosition(rb.position + move * speed * Time.deltaTime);
 
-        //rb.MovePosition(rb.position + move * speed * Time.deltaTime);
-        rb.velocity = new Vector3(move.x * speed, rb.velocity.y, move.z * speed);
+        // Utilisation du jetpack
+        if (!isGrounded && jetpackCharge > 0)
+        {
+            //OnJump();
+        }
 
+        // Respawn logic
         if (transform.position.y < -100)
         {
-            transform.position = respawnPoint.position;
+            Respawn();
         }
+
+        if (jetpack.action.IsPressed()){
+            UseJetpack();
+           }
     }
 
-    // Input handling methods
     private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
@@ -51,12 +71,23 @@ public class PlayerMovementsRB : MonoBehaviour
         movementY = movementVector.y;
     }
 
-    private void OnJump(InputValue jumpValue)
+    /*private void OnJump(InputValue jetpackValue)
     {
-        if (isGrounded)
+        float jetpackInput = jetpackValue.Get<float>();
+        if (jetpackInput > 0 && jetpackCharge > 0)
         {
-            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+            UseJetpack();
         }
+        else
+        {
+
+        }
+    }*/
+
+    private void UseJetpack()
+    {
+        rb.AddForce(Vector3.up * jetpackForce, ForceMode.Acceleration);
+        jetpackCharge = Mathf.Max(jetpackCharge - Time.deltaTime, 0);
     }
 
     /*void OnControllerColliderHit(ControllerColliderHit hit)
@@ -75,5 +106,10 @@ public class PlayerMovementsRB : MonoBehaviour
     {
         movementX = Input.GetAxis("Horizontal");
         movementY = Input.GetAxis("Vertical");
+    }
+
+    private void Respawn()
+    {
+        transform.position = respawnPoint.position;
     }
 }

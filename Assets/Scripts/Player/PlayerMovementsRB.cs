b7c32c49467c440c52be81e3ad7408fef3824482
Wood <wassim.boulatika@gmail.com>
2023-12-17 17:27:48 +0100

    Organized project

--- Assets/Scripts/Player/PlayerMovementsRB.cs
+++ Assets/Scripts/Player/PlayerMovementsRB.cs
@@ -5,24 +5,29 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovementsRB : MonoBehaviour
 {
+    [Header("Player Settings")]
     public float speed;
     public float jumpHeight;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-    public float bumperJumpHeight;
-    public Transform respawnPoint;
 
+    [Header("Respawn Settings")]
+    public Transform respawnPoint;
+    public float respawnPositionY;
+
+    [Header("Jetpack Settings")]
     public float jetpackForce = 10f;
     public float jetpackChargeRate = 1f;
     public float maxJetpackCharge = 100f;
+    public float jetpackCharge;
+    public InputActionReference jetpack;
 
     private Rigidbody rb;
     private float movementX;
     private float movementY;
     private bool isGrounded;
-    public float jetpackCharge;
-    public InputActionReference jetpack;
+    
 
     private void Start()
     {
@@ -35,10 +40,8 @@ public class PlayerMovementsRB : MonoBehaviour
     {
         GetPlayerInput();
 
-        // Check if grounded
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        // Recharge du jetpack au sol
         if (isGrounded)
         {
             jetpackCharge = Mathf.Min(jetpackCharge + jetpackChargeRate * Time.deltaTime, maxJetpackCharge);
@@ -47,14 +50,9 @@ public class PlayerMovementsRB : MonoBehaviour
         Vector3 move = transform.right * movementX + transform.forward * movementY;
         rb.MovePosition(rb.position + move * speed * Time.deltaTime);
 
-        // Utilisation du jetpack
-        if (!isGrounded && jetpackCharge > 0)
-        {
-            //OnJump();
-        }
 
         // Respawn logic
-        if (transform.position.y < -100)
+        if (transform.position.y < respawnPositionY)
         {
             Respawn();
         }
@@ -89,18 +87,6 @@ public class PlayerMovementsRB : MonoBehaviour
         rb.AddForce(Vector3.up * jetpackForce, ForceMode.Acceleration);
         jetpackCharge = Mathf.Max(jetpackCharge - Time.deltaTime, 0);
     }
-
-    /*void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        if (hit.collider.CompareTag("Bumper"))
-        {
-            float originalJumpHeight = jumpHeight;
-            jumpHeight = bumperJumpHeight;
-            // Code pour gérer le saut depuis le bumper
-            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
-            jumpHeight = originalJumpHeight;
-        }
-    }*/
 
     private void GetPlayerInput()
     {

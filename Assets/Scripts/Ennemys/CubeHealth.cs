800c69198a26e6d64beba9d26a11d841b9f6877a
Shaaken5 <casasof2011@hotmail.fr>
2023-12-27 16:36:59 +0100

    BlocDestroy Implémenté

--- Assets/Scripts/Ennemys/CubeHealth.cs
+++ Assets/Scripts/Ennemys/CubeHealth.cs
@@ -4,13 +4,14 @@ public class CubeHealth : MonoBehaviour
 {
     public int health = 1;
     public Material lvl1;
-    public Material lvl2; 
+    public Material lvl2;
     public Material lvl3;
     public Material lvl4;
-    public Material lvl5; 
+    public Material lvl5;
+    public GameObject destructionParticlesPrefab; // Ajout de la référence au système de particules
 
     private Renderer cubeRenderer;
-    private int previousHealth; 
+    private int previousHealth;
 
     private void Start()
     {
@@ -21,25 +22,25 @@ public class CubeHealth : MonoBehaviour
     private void Update()
     {
         UpdateMaterial();
-        CheckLevelDown(); 
-        CheckLevelUp(); 
+        CheckLevelDown();
+        CheckLevelUp();
         previousHealth = health;
     }
 
     public void TakeDamage(int damage)
-{
-    health -= damage;
-
-    if (health <= 0)
     {
-        FMODUnity.RuntimeManager.PlayOneShot("event:/DestructibleBlock/Behaviours/Destruction", GetComponent<Transform>().position);
-        Destroy(gameObject);
+        health -= damage;
+
+        if (health <= 0)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/DestructibleBlock/Behaviours/Destruction", transform.position);
+            SpawnDestructionParticles();
+            Destroy(gameObject);
+        }
     }
-}
 
     private void UpdateMaterial()
     {
-        
         if (health <= 1)
         {
             cubeRenderer.material = lvl1;
@@ -52,11 +53,11 @@ public class CubeHealth : MonoBehaviour
         {
             cubeRenderer.material = lvl3;
         }
-         else if (health <= 16)
+        else if (health <= 16)
         {
             cubeRenderer.material = lvl4;
         }
-         else if (health <= 25)
+        else if (health <= 25)
         {
             cubeRenderer.material = lvl5;
         }
@@ -73,38 +74,46 @@ public class CubeHealth : MonoBehaviour
         }
     }
 
-private void CheckLevelUp()
+    private void CheckLevelUp()
     {
         if (health > previousHealth)
         {
-                FMODUnity.RuntimeManager.PlayOneShot("event:/DestructibleBlock/Behaviours/MoreHP", GetComponent<Transform>().position);
+            FMODUnity.RuntimeManager.PlayOneShot("event:/DestructibleBlock/Behaviours/MoreHP", transform.position);
         }
 
         previousHealth = health;
     }
 
-// Fonction pour déterminer le niveau en fonction de la santé
-private int DetermineLevel(int healthValue)
-{
-    if (healthValue <= 1)
+    private int DetermineLevel(int healthValue)
     {
-        return 1;
+        if (healthValue <= 1)
+        {
+            return 1;
+        }
+        else if (healthValue <= 2)
+        {
+            return 2;
+        }
+        else if (healthValue <= 5)
+        {
+            return 3;
+        }
+        else if (healthValue <= 10)
+        {
+            return 4;
+        }
+        else
+        {
+            return 5;
+        }
     }
-    else if (healthValue <= 2)
+    private void SpawnDestructionParticles()
     {
-        return 2;
+        if (destructionParticlesPrefab != null)
+        {
+            GameObject particles = Instantiate(destructionParticlesPrefab, transform.position, Quaternion.identity);
+            Destroy(particles, 1f); // Détruire les particules après 1 seconde
+        }
     }
-    else if (healthValue <= 5)
-    {
-        return 3;
-    }
-    else if (healthValue <= 10)
-    {
-        return 4;
-    }
-    else
-    {
-        return 5;
-    }
-}
+
 }

270ba0b53df596fdaf924b837f48e5a33db40c8a
Wood <wassim.boulatika@gmail.com>
2023-12-30 18:06:46 +0100

    added explosive bullets boolean settings to gun controller

---
+++ Assets/Scripts/Weapons/ExplosiveBullet.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class ExplosiveBullet : MonoBehaviour
+{
+    private float explosionRadius;
+    private GameObject explosionPrefab;
+    private int bulletDamage;
+
+    public void InitializeExplosive(float radius, GameObject prefab, int damage)
+    {
+        explosionRadius = radius;
+        explosionPrefab = prefab;
+        bulletDamage = damage;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        CubeHealth cubeHealth = collision.gameObject.GetComponent<CubeHealth>();
+
+        if (cubeHealth != null)
+        {
+            cubeHealth.TakeDamage(bulletDamage);
+        }
+
+        Explode();
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        foreach (Collider collider in colliders)
+        {
+            CubeHealth cubeHealth = collider.GetComponent<CubeHealth>();
+
+            if (cubeHealth != null)
+            {
+                cubeHealth.TakeDamage(bulletDamage);
+            }
+        }
+
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}

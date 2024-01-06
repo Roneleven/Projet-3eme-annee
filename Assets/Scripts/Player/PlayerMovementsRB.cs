6438d438a37cc5888d812e2707a25506f9a3c766
Wood <wassim.boulatika@gmail.com>
2024-01-06 16:00:32 +0100

    fix commit

--- Assets/Scripts/Player/PlayerMovementsRB.cs
+++ Assets/Scripts/Player/PlayerMovementsRB.cs
@@ -31,12 +31,15 @@ public class PlayerMovementsRB : MonoBehaviour
     private float movementX;
     private float movementY;
     private bool isGrounded;
+    private FMOD.Studio.EventInstance jetUse;
+    private bool isJetUsePlaying = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         jetpackCharge = maxJetpackCharge;
+        jetUse = FMODUnity.RuntimeManager.CreateInstance("event:/Character/Locomotion/JetUse");
     }
 
     private void Update()
@@ -47,10 +50,22 @@ public class PlayerMovementsRB : MonoBehaviour
         {
             jetpackCharge = Mathf.Min(jetpackCharge + jetpackChargeRate * Time.deltaTime, maxJetpackCharge);
         }
-
-        if (jetpack.action.IsPressed())
+        if (jetpack.action.IsPressed() && jetpackCharge > 0)
         {
+            if (!isJetUsePlaying)
+            {
+                jetUse.start();
+                isJetUsePlaying = true;
+            }
             UseJetpack();
+        }
+        else
+        {
+            if (isJetUsePlaying)
+            {
+                jetUse.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                isJetUsePlaying = false;
+            }
         }
 
         UpdateJetpackChargeUI();
@@ -63,12 +78,19 @@ public class PlayerMovementsRB : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (jetUse.isValid())
+        {
+            FMOD.Studio.PLAYBACK_STATE state;
+            jetUse.getPlaybackState(out state);
+            isJetUsePlaying = state != FMOD.Studio.PLAYBACK_STATE.STOPPED;
+        }
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         Vector3 move = transform.right * movementX + transform.forward * movementY;
         rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
 
-        if (jetpack.action.IsPressed())
+        if (jetpack.action.IsPressed() && jetpackCharge > 0)
         {
             UseJetpack();
         }
@@ -77,7 +99,6 @@ public class PlayerMovementsRB : MonoBehaviour
         {
             FMODUnity.RuntimeManager.PlayOneShot("event:/Character/Locomotion/Footsteps");
         }
-
     }
 
     private void OnMove(InputValue movementValue)

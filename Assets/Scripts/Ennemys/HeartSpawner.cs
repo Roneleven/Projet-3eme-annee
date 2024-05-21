d695a695ea3980ad23d8bd308d21663ac1a15c98
Wood <wassim.boulatika@gmail.com>
2024-05-21 12:14:06 +0200

    fix heartspawner bug and added boxspawner to index 0

--- Assets/Scripts/Ennemys/HeartSpawner.cs
+++ Assets/Scripts/Ennemys/HeartSpawner.cs
@@ -32,6 +32,7 @@ public class HeartSpawner : MonoBehaviour
     public event PalierChangeAction OnPalierChange;
     public GameObject coconvfx;
 
+
     private FMOD.Studio.EventInstance BreakingHeart;
 
     [Header("Timer/Reset Properties")]
@@ -41,12 +42,12 @@ public class HeartSpawner : MonoBehaviour
     public TextMeshProUGUI timerText;
     public Image blackFade;
     public Animator anim;
-    public Image timerFillImage; // New UI Image for timer representation
 
     private bool isCooldownActive = false;
 
     private bool playerInPosition;
     public Vector3 playerPosition;
+
 
     [Header("Throw cube pattern Properties")]
     public float cubeDestroyDelay;
@@ -65,6 +66,7 @@ public class HeartSpawner : MonoBehaviour
     public float cageTransparentScale;
     private FMOD.Studio.EventInstance warning;
 
+
     [Header("Patterns Properties")]
     public CubeLauncherPattern cubeLauncherPattern;
     public CubeTracking cubeTrackingScript;
@@ -75,6 +77,15 @@ public class HeartSpawner : MonoBehaviour
     public GatlinLauncherPattern gatlinLauncherPattern;
 
     public BossPatternManager bossPatternManager;
+    //creer une classeliste de palier (donc 10), faire une boucle dans le start pour un new palier et inserer dans la liste
+    //car la liste creer pas les objets tous seul, il faut en gros l'initialisé, en faite mettre en public
+    //classe palier system seriazable, dans cette classe liste de pattern
+    //creer enum de pattern (tous les pattern disponible) commec a palier font ref à x éléments de cet énum
+    //switchcurrentpalier etc la fonction, prendre la liste de palier, indexer avec current palier, pour recup le palier et faire .pattern pour avoir la liste des patterns
+    //on choisis un au hasard par exemple, une fois choisis on fait un switch qui va ressembler a ce que j'avais en un peu moins lourd
+    // case bigwall,case cubelauncher etc si on est dans le case cubetracking on startcoroutine cubetracking, et c'est déclencher en fonction du palier ou on est
+    //dans hearthealth mettre une list de pattern pour les exclusions, scrap la data et le code sera dans switch to next pattern en gros, c'est elle qui lance les pattern et recupere tout. random ou algo pour choisir qui appelle la bonne coroutine
+
 
     private void Start()
     {
@@ -88,17 +99,21 @@ public class HeartSpawner : MonoBehaviour
         bossPatternManager = GetComponent<BossPatternManager>();
         heartHealth = GetComponent<HeartHealth>();
         warning = FMODUnity.RuntimeManager.CreateInstance("event:/Heart/Patterns/Cage_Warning");
-        BreakingHeart = FMODUnity.RuntimeManager.CreateInstance("event:/V1/UX/Ambience/CoreBreaked");  // Integrate music here
+        //BreakingHeart = FMODUnity.RuntimeManager.CreateInstance("event:/V1/UX/Ambience/CoreBreaked");  INTéGRER LA MUSIQUE ICI
         BreakingHeart.start();
+
+        //dissolve = gameObject.GetComponent<Animation>();
 
         StartCoroutine(SpawnCube());
         bossPatternManager.StartPatternsForCurrentPalier();
     }
 
+
     public void OnCurrentPalierChanged()
     {
         bossPatternManager.StartPatternsForCurrentPalier();
     }
+
 
     private void Update()
     {
@@ -111,15 +126,7 @@ public class HeartSpawner : MonoBehaviour
             }
             else
             {
-                // Calculate percentage completed
-                float percentageCompleted = (1 - (timer / defaultTimer)) * 100;
-                timerText.text = Mathf.RoundToInt(percentageCompleted).ToString() + "%";
-
-                if (timerFillImage != null)
-                {
-                    // Fill amount based on the percentage completed
-                    timerFillImage.fillAmount = 1 - (timer / defaultTimer);
-                }
+                timerText.text = Mathf.Round(timer).ToString() + "s";
             }
         }
 
@@ -214,7 +221,6 @@ public class HeartSpawner : MonoBehaviour
             yield return new WaitForSeconds(spawnInterval);
         }
     }
-    
     IEnumerator PlayAnimationAndReload()
     {
         anim.Play("FadeIn");
@@ -246,6 +252,10 @@ public class HeartSpawner : MonoBehaviour
     private IEnumerator SpawnTransparentAndRealCube(Vector3 spawnPosition)
     {
         GameObject transparentCube = Instantiate(transparentCubePrefab, spawnPosition, Quaternion.identity, spawnContainer.transform);
+
+        //GameObject transparentCube = transparentCubesPool.GetObject(); nstantiate(transparentCubePrefab, spawnPosition, Quaternion.identity, spawnContainer.transform);
+        // transparentCube.transform.position = spawnPosition;
+        // transparentCube.transform.setParent(spawnContainer.transform);
         yield return new WaitForSeconds(spawnInterval);
 
         Collider[] colliders = Physics.OverlapSphere(spawnPosition, gridSize / 2);
@@ -299,74 +309,181 @@ public class HeartSpawner : MonoBehaviour
 
     private void UpdateCocon()
     {
-        cocon = currentPalier * 0.2f + 2;
+        cocon = (spawnRadius + 2) * 110;
         coconvfx.transform.localScale = new Vector3(cocon, cocon, cocon);
     }
 
     private IEnumerator ResetPalier()
     {
-        previousPalier = currentPalier;
-        currentPalier++;
-        UpdateCocon();
+        isCooldownActive = true;
 
-        // Augmenter le nombre de cubes à spawner pendant la durée temporaire
+        float originalSpawnRadius = spawnRadius;
+        float originalSpawnCount = spawnCount;
+        float originalSpawnInterval = spawnInterval;
+
         spawnCount = temporarySpawnCount;
         spawnInterval = temporarySpawnInterval;
 
-        // Attendre la durée temporaire
-        yield return new WaitForSeconds(timeTemporaryPalier);
+        previousPalier = currentPalier;
 
-        // Revenir aux valeurs normales
-        spawnCount = Mathf.RoundToInt(spawnCount * (1.2f + (currentPalier * 0.1f)));
-        spawnInterval = spawnInterval * (0.9f - (currentPalier * 0.05f));
+        for (int palier = 1; palier <= currentPalier; palier++)
+        {
+            float temporarySpawnCountBeforeAdjust = spawnCount;
+            AdjustPalierValues(palier);
+            spawnCount = temporarySpawnCountBeforeAdjust;
+            yield return new WaitForSeconds(timeTemporaryPalier);
+        }
 
-        // Notifier le changement de palier
+        // Restaurer les valeurs originales
+        spawnRadius = originalSpawnRadius;
+        spawnCount = originalSpawnCount;
+        spawnInterval = originalSpawnInterval;
+
+        // Passer au palier suivant
+        currentPalier++;
         OnCurrentPalierChanged();
+
+        if (currentPalier <= maxPalier)
+        {
+            AdjustPalierValues(currentPalier);
+        }
+
+        if (currentPalier >= maxPalier && !timerActive)
+        {
+            timerActive = true;
+            Invoke("TimeOut", timer);
+        }
+
+        isCooldownActive = false;
     }
+
+    private void AdjustPalierValues(int palier)
+    {
+        float levelUpIncrement = 1.0f;
+
+        spawnRadius = palier * 4;
+        UpdateCocon();
+
+        if (palier == 1)
+        {
+            spawnCount = 6;
+        }
+        else if (palier == 2)
+        {
+            spawnCount = 6 + ((palier - 1) * 4);
+
+        }
+
+        float newLevelUpValue = palier * levelUpIncrement;
+
+        // Déclencher l'événement OnPalierChange avec le nouveau palier
+        if (OnPalierChange != null)
+        {
+            OnPalierChange(palier);
+        }
+    }
+
 
     private IEnumerator ChangePalierAutomatically()
     {
         if (currentPalier < maxPalier)
         {
             currentPalier++;
-            timeSincePalierStart = 0f;
-            timeNextPalier += timeTemporaryPalier;
-
-            float newLevelUpValue = (currentPalier + 1) * 1.0f;
-            BreakingHeart.setParameterByName("LevelUp 2", newLevelUpValue);
-
-            // Déclencher l'événement OnPalierChange avec le nouveau palier
-            if (OnPalierChange != null)
-            {
-                OnPalierChange(currentPalier);
-            }
-
-            yield return new WaitForSeconds(timeTemporaryPalier);
-
             OnCurrentPalierChanged();
+
+            AdjustPalierValues(currentPalier);
+
+            // Réinitialiser le temps pour le nouveau palier
+            timeSincePalierStart = 0f;
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Heart/Behaviours/AutoLevelUp");
+
+            // Attendre avant de déclencher à nouveau le changement automatique (30 secondes)
+            yield return new WaitForSeconds(timeNextPalier);
+
+            // Appeler récursivement la fonction pour le palier suivant
+            StartCoroutine(ChangePalierAutomatically());
+        }
+        else
+        {
+            // Si nous avons atteint le dernier palier, activer le chronomètre pour le timeout
+            timerActive = true;
+            Invoke("TimeOut", timer);
         }
     }
+
     #endregion
 
-    #region CAGE TRACKING
-    public IEnumerator GenerateCagePattern()
+    #region BOSS PATTERNS
+    private IEnumerator GenerateCagePattern()
     {
-        // Preview setup
-        GameObject preview = Instantiate(cagePatternPreview, playerPosition, Quaternion.identity);
-        preview.transform.localScale = Vector3.one * cageTransparentScale;
-        preview.GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0, 0.5f);
-        warning.start();
-
-        yield return new WaitForSeconds(cageSpawnTime);
-
-        Destroy(preview);
-
-        GameObject cage = Instantiate(CageBlockPrefab, playerPosition, Quaternion.identity);
-        cage.transform.localScale = Vector3.one * cageRadius;
         cagePatternActive = true;
 
-        yield return new WaitForSeconds(3f); // Duration of the pattern
+        Vector3 playerGridPosition = playerPosition / gridSize;
+        playerGridPosition = new Vector3(Mathf.Round(playerGridPosition.x), Mathf.Round(playerGridPosition.y), Mathf.Round(playerGridPosition.z));
+        playerGridPosition *= gridSize;
+
+        int cageSizeXZ = 3;
+
+        // Générer un seul cube transparent à une échelle plus grande
+        GameObject transparentCube = Instantiate(cagePatternPreview, playerGridPosition, Quaternion.identity, spawnContainer.transform);
+        transparentCube.transform.localScale = new Vector3(cageTransparentScale, cageTransparentScale, cageTransparentScale);
+        warning.setParameterByName("Cage", 0.0F);
+        warning.start();
+
+        float timer = 0f;
+        bool playerExited = false;
+
+        while (timer < cageSpawnTime)
+        {
+            timer += Time.deltaTime;
+            float distanceToPlayer = Vector3.Distance(playerPosition, playerGridPosition);
+            float acceptableOverlap = gridSize / 0.4f;
+
+            if (distanceToPlayer >= (gridSize + acceptableOverlap))
+            {
+                playerExited = true;
+                break;
+            }
+
+            yield return null;
+        }
+
+        if (!playerExited)
+        {
+            warning.setParameterByName("Cage", 1.0F);
+            // Générer la cage autour du joueur
+            for (float x = playerGridPosition.x - cageSizeXZ; x <= playerGridPosition.x + cageSizeXZ; x += gridSize)
+            {
+                for (float y = playerGridPosition.y - cageSizeXZ; y <= playerGridPosition.y + cageSizeXZ; y += gridSize)
+                {
+                    for (float z = playerGridPosition.z - cageSizeXZ; z <= playerGridPosition.z + cageSizeXZ; z += gridSize)
+                    {
+                        Vector3 cageSpawnPosition = new Vector3(x, y, z);
+                        if (Mathf.Abs(x - playerGridPosition.x) >= cageSizeXZ || Mathf.Abs(y - playerGridPosition.y) >= cageSizeXZ || Mathf.Abs(z - playerGridPosition.z) >= cageSizeXZ)
+                        {
+                            Instantiate(CageBlockPrefab, cageSpawnPosition, Quaternion.identity, spawnContainer.transform);
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            warning.setParameterByName("Cage", 2.0F);
+        }
+
+        // Détruire le cube transparent car la cage a été générée ou le joueur a quitté
+        Destroy(transparentCube);
+
         cagePatternActive = false;
     }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, spawnRadius * cageRadius);
+    }
     #endregion
+
 }
